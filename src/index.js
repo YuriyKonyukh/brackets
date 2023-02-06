ebee0d838d01d6ccfdba823103de01d8b513d79f
Yuriy <yuriykonyukh@gmail.com>
2023-02-06 11:41:18 +0300

    feat: add solution for task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+// your solution
+str = str.split('');
+console.log(str)
+function findBrackets() {
+  for (let i = 0; i < str.length; i++) {
+    bracketsConfig.forEach(item => {
+      if (item[0] == str[i] && item[1] == str[i + 1]) {str.splice(i, 2);
+        if (bracketsConfig.length > 0) {findBrackets()}};
+    });
+  }
+  return str;
 }
+findBrackets();
+if (str.length > 0) {return false
+} else return true;}
